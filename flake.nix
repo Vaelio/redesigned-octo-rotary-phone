b1cf1492335e53e12d6a306f73f50b03975b1e5e
vaelio <valentin.eudeline@gmail.com>
2025-06-23 17:41:47 +0200

    Added a nix develop command to allow to use local image

--- flake.nix
+++ flake.nix
@@ -8,6 +8,50 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       # A wrapper script that mounts ./data and runs the image
+      runDevScript = pkgs.writeShellScriptBin "run-my-dev-container" ''
+        if [ $# -ge 1 ]; then
+            CTR_NAME="formol-$1"
+	else
+	    rand="$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c5)"
+	    CTR_NAME="formol-$rand"
+	fi
+	if sudo docker container inspect "$CTR_NAME" >/dev/null 2>&1; then
+            echo "[-] Skipping container creation"
+        else
+	    if [ ! -d ./workspace ]; then
+              mkdir ./workspace
+	    fi
+            echo "[+] Creating $CTR_NAME container..."
+            sudo docker create \
+	      --network=host \
+              --name $CTR_NAME \
+              --hostname $CTR_NAME \
+	      -e HOME=/root \
+	      -e SHELL=zsh \
+	      -e DISPLAY=:0 \
+	      -e _JAVA_AWT_WM_NONREPARENTING=1 \
+	      -e QT_X11_NO_MITSHM=1 \
+              -e ZSH_THEME=gentoo \
+	      --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix \
+              --mount type=bind,src=/etc/localtime,dst=/etc/localtime,readonly=true \
+	      --mount type=bind,src=./workspace,dst=/workspace \
+	      nix-pentest-ctr \
+	      /bin/bash /bin/entrypoint.sh endless
+              sleep 1
+	      echo "[+] Creating workspace..."
+	      sudo docker exec -ti "$CTR_NAME" mkdir /workspace
+	fi
+	if sudo docker ps --filter "name=$CTR_NAME" --filter "status=running" --format '{{.Names}}' | grep -qx "$CTR_NAME"; then
+              echo "[-] Container already started"
+          else
+              echo "[+] Starting the container..."
+              sudo docker start "$CTR_NAME"
+              sleep 2
+          fi
+	  
+          echo "🚀 Exec-ing inside the container..."
+          sudo docker exec -w /workspace -e SHELL=/bin/zsh -ti "$CTR_NAME" zsh -i
+      '';
       runScript = pkgs.writeShellScriptBin "run-my-container" ''
         if [ $# -ge 1 ]; then
             CTR_NAME="formol-$1"
@@ -56,9 +100,10 @@
         program = "${runScript}/bin/run-my-container";
       };
 
+
       # Optional: nix develop gives access to run script
       devShells.${system}.default = pkgs.mkShell {
-        packages = [ runScript ];
+        packages = [ runDevScript ];
       };
     };
 }
