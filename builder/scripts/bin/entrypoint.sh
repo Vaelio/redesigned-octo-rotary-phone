7f3fb829c954b3d7fa5b37f97e10b33c7b3ac981
vaelio <valentin.eudeline@gmail.com>
2025-06-24 15:50:53 +0200

    changed a little bit the entrypoint

--- builder/scripts/bin/entrypoint.sh
+++ builder/scripts/bin/entrypoint.sh
@@ -10,6 +10,7 @@ function setup_channels {
     nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
     nix-channel --update
     nix-shell '<home-manager>' -A install
+    mkdir /root/.config/home-manager -p
     
 }
 
