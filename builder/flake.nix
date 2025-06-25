21b1dda83b90b5526bcbedc64336bce72fb71456
vaelio <valentin.eudeline@gmail.com>
2025-06-25 11:10:20 +0200

    Added some pkgs

--- builder/flake.nix
+++ builder/flake.nix
@@ -14,10 +14,11 @@
         inherit system;
 	config.allowUnfree = true;
       };
-      linux-tools = with pkgs; [ coreutils-full lsd zsh bash oh-my-zsh fzf zellij curl wget nano vim git iconv tmux zsh-z zsh-autosuggestions zsh-completions zsh-syntax-highlighting python312 findutils gash-utils procps nix cacert su];
+      linux-tools = with pkgs; [ coreutils-full lsd zsh bash oh-my-zsh fzf zellij curl wget nano vim git iconv tmux zsh-z zsh-autosuggestions zsh-completions zsh-syntax-highlighting python312 findutils gash-utils procps nix cacert su python312Packages.ipython];
+      android = with pkgs; [android-tools];
       ctr-tools = with pkgs.dockerTools; [ usrBinEnv binSh caCertificates fakeNss];
-      ad-tools = with pkgs; [ netexec smbclient-ng samdump2 nbtscan openldap pretender onesixtyone sccmhunter krb5 responder mitm6 python312Packages.impacket python312Packages.lsassy bloodhound bloodhound-py neo4j python312Packages.ldapdomaindump];
-      network-tools = with pkgs; [ nmap proxychains netcat socat simple-http-server ];
+      ad-tools = with pkgs; [ netexec smbclient-ng samdump2 nbtscan openldap pretender onesixtyone sccmhunter krb5 responder mitm6 python312Packages.impacket python312Packages.lsassy bloodhound bloodhound-py neo4j python312Packages.ldapdomaindump python313Packages.certipy ldeep ];
+      network-tools = with pkgs; [ nmap proxychains-ng netcat socat simple-http-server ];
       pwn-tools = with pkgs; [ gdb gef nasm ropgadget python312Packages.ropper pwntools cutter rocmPackages.llvm.clang ];
       nix-pentest = pkgs.dockerTools.buildLayeredImage {
         name = "nix-pentest-ctr";
@@ -29,7 +30,9 @@
 	  Env = [ "HOME=/root" "ZSH_THEME=gentoo" "USER=root"];
         };
 	contents = with pkgs; [
+	  ./root
 	  ./scripts
+	  android
 	  ctr-tools
 	  linux-tools
 	  ad-tools
