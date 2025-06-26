bd6d5955ecdbe17ee4081f09ccd068d54573beff
vaelio <valentin.eudeline@gmail.com>
2025-06-26 11:53:12 +0200

    flake.nix cleanup + reducing the number of layers via maxLayers and symlinkJoin

--- builder/flake.nix
+++ builder/flake.nix
@@ -2,24 +2,39 @@
   description = "Docker image with runtime mounts via nix run";
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/25.05";
-      #rustyproxy = {
-      #  url = "gitlab:r2367/RustyProxy";
-      #};
   };
 
-  outputs = { self, nixpkgs}: #, rustyproxy
+  outputs = { self, nixpkgs}:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { 
         inherit system;
 	config.allowUnfree = true;
       };
-      linux-tools = with pkgs; [ coreutils-full lsd zsh bash oh-my-zsh fzf zellij curl wget nano vim git iconv tmux zsh-z zsh-autosuggestions zsh-completions zsh-syntax-highlighting python312 findutils gash-utils procps nix cacert su python312Packages.ipython];
-      android = with pkgs; [android-tools];
-      ctr-tools = with pkgs.dockerTools; [ usrBinEnv binSh caCertificates fakeNss];
-      ad-tools = with pkgs; [ netexec smbclient-ng samdump2 nbtscan openldap pretender onesixtyone sccmhunter krb5 responder mitm6 python312Packages.impacket python312Packages.lsassy bloodhound bloodhound-py neo4j python312Packages.ldapdomaindump python313Packages.certipy ldeep ];
-      network-tools = with pkgs; [ nmap proxychains-ng netcat socat simple-http-server ];
-      pwn-tools = with pkgs; [ gdb gef nasm ropgadget python312Packages.ropper pwntools cutter rocmPackages.llvm.clang ];
+      linux-tools = with pkgs; [ (pkgs.symlinkJoin {
+        name = "linux-tools";
+	paths = [ coreutils-full lsd zsh bash oh-my-zsh fzf zellij curl wget nano vim git iconv tmux zsh-z zsh-autosuggestions zsh-completions zsh-syntax-highlighting python312 findutils gash-utils procps nix cacert su python312Packages.ipython ];
+      })];
+      android = with pkgs; [ (pkgs.symlinkJoin {
+        name = "android";
+	paths = [ android-tools ];
+      })];
+      ctr-tools = with pkgs.dockerTools; [ (pkgs.symlinkJoin {
+        name = "ctr-tools";
+	paths = [ usrBinEnv binSh caCertificates fakeNss ];
+      })];
+      ad-tools = with pkgs; [ (pkgs.symlinkJoin {
+        name = "ad-tools";
+	paths = [ netexec smbclient-ng samdump2 nbtscan openldap pretender onesixtyone sccmhunter krb5 responder mitm6 python312Packages.impacket python312Packages.lsassy bloodhound bloodhound-py neo4j python312Packages.ldapdomaindump python313Packages.certipy ldeep ];
+      })];
+      network-tools = with pkgs; [ (pkgs.symlinkJoin { 
+        name = "network-tools";
+	paths = [ nmap proxychains-ng netcat socat simple-http-server wireshark];
+      })];
+      pwn-tools = with pkgs; [ (pkgs.symlinkJoin {
+        name = "pwn-tools";
+	paths = [ gdb gef nasm ropgadget python312Packages.ropper pwntool cutter rocmPackages.llvm.clang ];
+      })];
       nix-pentest = pkgs.dockerTools.buildLayeredImage {
         name = "nix-pentest-ctr";
         tag = "latest";
@@ -38,6 +53,7 @@
 	  network-tools
 	  shadow
 	];
+	maxLayers = 10;
 	extraCommands = ''
 	  mkdir -p ./tmp
 	'';
