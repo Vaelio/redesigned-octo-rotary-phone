48602a3b257a7f0daf1b1b099b708be16dbb0338
vaelio <valentin.eudeline@gmail.com>
2025-07-22 12:08:11 +0200

    More tools. lets hope the CI doesnt get full

--- builder/flake.nix
+++ builder/flake.nix
@@ -34,13 +34,25 @@
       linux-tools = with pkgs; [
         (pkgs.symlinkJoin {
           name = "linux-tools";
-          paths = [ coreutils-full lsd zsh bash oh-my-zsh fzf zellij curl wget nano vim git iconv tmux zsh-z zsh-autosuggestions zsh-completions zsh-syntax-highlighting python312 findutils procps nix cacert su python312Packages.ipython unixtools.script less iproute2 unixtools.netstat bottom htop gnugrep gawkInteractive gnused ncurses];
+          paths = [ coreutils-full lsd zsh bash oh-my-zsh fzf zellij curl wget nano vim git iconv tmux zsh-z zsh-autosuggestions zsh-completions zsh-syntax-highlighting python312 findutils procps nix cacert su python312Packages.ipython unixtools.script less iproute2 unixtools.netstat bottom htop gnugrep gawkInteractive gnused ncurses unixtools.xxd ];
         })
       ];
       web-tools = with pkgs; [
         (pkgs.symlinkJoin {
           name = "web-tools";
-          paths = [ feroxbuster seclists ffuf sslscan nuclei soapui sqlmap subfinder testssl wafw00f waybackurls wfuzz whatweb whois wpscan ];
+          paths = [ feroxbuster seclists ffuf sslscan nuclei soapui sqlmap subfinder testssl wafw00f waybackurls wfuzz whatweb whois wpscan arjun assetfinder dirb dirsearch dnsenum dnsx gau gobuster hakrawler hping httprobe httpx joomscan jwt-cli katana ngrok scout testssl ];
+        })
+      ];
+      generic-tools = with pkgs; [
+        (pkgs.symlinjJoin {
+          name = "generic-tools";
+          paths = [ exiftool dex2jar fcrackzip gron hashcat hexedit john ligolo-ng pdfcrack ];
+        })
+      ];
+      cloud-tools = with pkgs; [
+        (pkgs.symlinkJoin {
+          name = "cloud-tools";
+          paths = [ awscli2 azure-cli kubectl ];
         })
       ];
       android = with pkgs; [
@@ -55,22 +67,28 @@
           paths = [ usrBinEnv binSh caCertificates fakeNss ];
         })
       ];
+      code-tools = with pkgs.dockerTools; [
+        (pkgs.symlinkJoin {
+          name = "code-tools";
+          paths = [ semgrep gitleaks trufflehog ];
+        })
+      ];
       ad-tools = with pkgs; [
         (pkgs.symlinkJoin {
           name = "ad-tools";
-          paths = [ netexec smbclient-ng samdump2 nbtscan openldap pretender onesixtyone sccmhunter krb5 responder mitm6 python312Packages.impacket python312Packages.lsassy bloodhound bloodhound-py neo4j python312Packages.ldapdomaindump python313Packages.certipy ldeep manspider asrepcatcher legba mariadb masscan metasploit mitmproxy netdiscover exploitdb sshuttle swaks freerdp smbmap enum4linux enum4linux-ng pylaps ];
+          paths = [ netexec smbclient-ng samdump2 nbtscan openldap pretender onesixtyone sccmhunter krb5 responder mitm6 python312Packages.impacket python312Packages.lsassy bloodhound bloodhound-py neo4j python312Packages.ldapdomaindump python312Packages.certipy ldeep manspider asrepcatcher legba mariadb masscan metasploit mitmproxy netdiscover exploitdb sshuttle swaks freerdp smbmap enum4linux enum4linux-ng pylaps python312Packages.pypykatz krbjack libmspack polenum coercer donpapi certsync keepwn pre2k python312Packages.masky python312Packages.pywerview autobloody python312Packages.dploot amass bettercap cewl eyewitness gotwitness ];
         })
       ];
       network-tools = with pkgs; [
         (pkgs.symlinkJoin {
           name = "network-tools";
-          paths = [ nmap proxychains-ng netcat socat simple-http-server wireshark openssh proxmark3 rdesktop rsync tcpdump tshark traceroute wifite2 ];
+          paths = [ nmap proxychains-ng netcat socat simple-http-server wireshark openssh proxmark3 rdesktop rsync tcpdump tshark traceroute wifite2 aircrack-ng chisel tigervnc ];
         })
       ];
       pwn-tools = with pkgs; [
         (pkgs.symlinkJoin {
           name = "pwn-tools";
-          paths = [ gdb gef nasm ropgadget python312Packages.ropper pwntool cutter rocmPackages.llvm.clang ];
+          paths = [ gdb gef nasm ropgadget python312Packages.ropper pwntools cutter rocmPackages.llvm.clang binwalk checksec ];
         })
       ];
       nix-pentest = pkgs.dockerTools.buildLayeredImage {
@@ -89,10 +107,13 @@
           ./fs
           android
           ctr-tools
+          code-tools
           linux-tools
           ad-tools
           network-tools
+          generic-tools
           web-tools
+          cloud-tools
           shadow
           extraLayer
         ];
